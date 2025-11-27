146534fa753b707d4034d553b91d04cb37856cd0
mindblendstudios <mindblendstudios@gmail.com>
2025-11-27 11:14:20 +0530

    Update ocrtextextraction.py
    use_container_width removed

--- ocrtextextraction.py
+++ ocrtextextraction.py
@@ -9,7 +9,7 @@ def run_ocr():
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
     if uploaded_file:
         image = Image.open(uploaded_file).convert("RGB")
-        st.image(image, caption="Uploaded Image", use_container_width=True)
+        st.image(image, caption="Uploaded Image")
 
         reader = easyocr.Reader(['en'])  # Initialize once; add more languages if needed
         results = reader.readtext(np.array(image))
